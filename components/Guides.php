01bc9b4a1cc9791b1079ddd00d46b6272c7f9671
Paul Lovato <plovato@gmail.com>
2019-01-31 18:32:58 -0700

    Guide roles support.

--- components/Guides.php
+++ components/Guides.php
@@ -6,6 +6,8 @@ use Cms\Classes\ComponentBase;
 
 class Guides extends ComponentBase
 {
+    private $jobs;
+
     public function componentDetails()
     {
         return [
@@ -14,33 +16,75 @@ class Guides extends ComponentBase
         ];
     }
 
+    public function defineProperties()
+    {
+        return [
+            'slug' => [
+                'title'       => 'Role Filter',
+                'description' => 'Enter a role slug. Leave empty to show all jobs.',
+                'type'        => 'string',
+                'default'     => ''
+            ]
+        ];
+    }
+
     public function onRun()
     {
-        $this->page['jobs'] = $this->getJobsList();
+        $this->prepareVars();
+    }
+
+    private function prepareVars()
+    {
+        $this->jobs = $this->page['jobs'] = $this->loadRole();
+    }
+
+    private function loadRole()
+    {
+        if (!$roleSlug = $this->property('slug'))
+            return $this->getJobsList();
+
+        if (!$jobs = $this->getJobs($roleSlug))
+            return null;
+
+        return $jobs;
     }
 
     private function getJobsList()
     {
         return [
-            ['name' => 'Paladin', 'abbr' => 'pld'],
-            ['name' => 'Warrior', 'abbr' => 'war'],
-            ['name' => 'Dark Knight', 'abbr' => 'drk'],
+            ['name' => 'Paladin', 'abbr' => 'pld', 'role' => 'tank'],
+            ['name' => 'Warrior', 'abbr' => 'war', 'role' => 'tank'],
+            ['name' => 'Dark Knight', 'abbr' => 'drk', 'role' => 'tank'],
 
-            ['name' => 'Monk', 'abbr' => 'mnk'],
-            ['name' => 'Dragoon', 'abbr' => 'drg'],
-            ['name' => 'Ninja', 'abbr' => 'nin'],
-            ['name' => 'Samurai', 'abbr' => 'sam'],
+            ['name' => 'Monk', 'abbr' => 'mnk', 'role' => 'melee'],
+            ['name' => 'Dragoon', 'abbr' => 'drg', 'role' => 'melee'],
+            ['name' => 'Ninja', 'abbr' => 'nin', 'role' => 'melee'],
+            ['name' => 'Samurai', 'abbr' => 'sam', 'role' => 'melee'],
 
-            ['name' => 'Bard', 'abbr' => 'brd'],
-            ['name' => 'Machinist', 'abbr' => 'mch'],
+            ['name' => 'Bard', 'abbr' => 'brd', 'role' => 'ranged'],
+            ['name' => 'Machinist', 'abbr' => 'mch', 'role' => 'ranged'],
 
-            ['name' => 'Black Mage', 'abbr' => 'blm'],
-            ['name' => 'Summoner', 'abbr' => 'smn'],
-            ['name' => 'Red Mage', 'abbr' => 'rdm'],
+            ['name' => 'Black Mage', 'abbr' => 'blm', 'role' => 'ranged'],
+            ['name' => 'Summoner', 'abbr' => 'smn', 'role' => 'ranged'],
+            ['name' => 'Red Mage', 'abbr' => 'rdm', 'role' => 'ranged'],
 
-            ['name' => 'White Mage', 'abbr' => 'whm'],
-            ['name' => 'Scholar', 'abbr' => 'sch'],
-            ['name' => 'Astrologian', 'abbr' => 'ast']
+            ['name' => 'White Mage', 'abbr' => 'whm', 'role' => 'healer'],
+            ['name' => 'Scholar', 'abbr' => 'sch', 'role' => 'healer'],
+            ['name' => 'Astrologian', 'abbr' => 'ast', 'role' => 'healer']
         ];
+    }
+
+    private function getJobs($role = '')
+    {
+        $jobs = $this->getJobsList();
+
+        $guideJobs = [];
+        foreach ($jobs as $job) {
+            if ($job['role'] === $role) {
+                $guideJobs[] = $job;
+            }
+        }
+
+        return $guideJobs;
     }
 }
